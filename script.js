c125a522420c65aa90533194f5e08d9ead5805a2
tj937 <zavriahaudraytejada@gmail.com>
2026-08-03 16:14:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,108 @@
 const output=document.getElementById("output");
 const input=document.getElementById("answer");
-
 let stage=0;
 
 const finalMessage=`Hello Cln!
 
-I just wanted to tell you now that I find you cute and attractive...
+If you're reading this...
+thank you for saying "yes."
+
+I've been wanting to tell you something for quite a while now, but I could never find the right timing... or maybe I just didn't have enough courage.
+
+So... here I am.
+
+I just wanted you to know that I genuinely think you're cute... and really attractive.
+
+At first, I thought it was just a simple crush that would disappear after a while.
+
+But the more I got to know you...
+
+the more I noticed the little things about you.
+
+The way you talk.
+
+The way you laugh.
+
+The way you smile.
+
+The way you carry yourself.
+
+The little things you probably don't even realize you do.
+
+Somehow... 
+they all stayed in my mind.
 
 I have to admit...
 
-I can't stop thinking about you...
+I can't stop thinking about you.
 
-Guess you've captured my thoughts completely.
+You've completely filled my thoughts.
 
-And..............................
+And....................................
 
 Iiiii....
 
 I like you.
 
-But...
+Not just because you're pretty.
 
-I also like your flaws...
+Not just because you're cute.
 
-your imperfections...
+I like your flaws.
 
-your personality...
+Your imperfections.
 
-your vibe...
+Your personality.
 
-your style...
+Your vibe.
 
-your worst...
+Your style.
 
-and most of all...
+Your humor.
 
-your smile!
+Your random moments.
 
-But no pressure.
+Even the parts of yourself that you might not like...
 
-You don't have to reciprocate my feelings, Sharlene.
+I think they make you who you are.
 
-I just needed to get this out of my head for my own peace of mind.
+And honestly...
 
-Kasi nakaka baliw na yung kagandahan mo.
+that's someone I really admire.
 
-CHAROT HAHAHA
+I know this might come as a surprise.
+
+Maybe you expected this...
+
+or maybe you didn't.
+
+Either way, I don't want you to feel pressured.
+
+You don't have to say yes.
+
+You don't have to force yourself to feel the same way.
+
+I just wanted to be honest with you because keeping these feelings to myself became harder than telling you.
+
+Whatever your answer is...
+
+I'll respect it.
+
+I won't be upset.
+
+I won't think badly of you.
+
+I just hope this message could at least make you smile.
+
+Thank you for taking the time to read everything.
+
+And...
+
+thank you for simply being you.
+
+Kasi nakaka baliw na yung kagandahan mo... CHAROT HAHAHA
+
+Take care, always..!
 
 -Zav`;
 
